a2c043af1d2602737bded6bcf76cb38dbe99833c
Gabriel Junio <gabriel.junio@rocketmail.com>
2022-08-08 16:23:31 -0300

    swagger config files

---
+++ src/Api/Configs/ApiConfig.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Configs
+{
+    public static class ApiConfig
+    {
+        public static IServiceCollection AddApiConfig(this IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddApiVersioning(options =>
+            {
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.DefaultApiVersion = new ApiVersion(1, 0);
+                options.ReportApiVersions = true;
+            });
+            services.AddVersionedApiExplorer(options =>
+            {
+                options.GroupNameFormat = "'v'VVV";
+                options.SubstituteApiVersionInUrl = true;
+            });
+
+            return services;
+        }
+    }
+}
---
+++ src/Api/Configs/Swagger/SwaggerConfig.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.OpenApi.Models;
+
+
+namespace Api.Config.Swagger
+{
+    public static class SwaggerConfig
+    {
+        /* Adding Swagger Configs */
+        public static IServiceCollection AddSwaggerConfig( this IServiceCollection services )
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.OperationFilter<SwaggerDefaultValues>();
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "Insira o token JWT desta maneira: Bearer {seu token}",
+                    Name = "Authorization",
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
+            });
+
+            return services;
+        }
+
+        /* Using Swagger Configs */
+        public static IApplicationBuilder UseSwaggerConfig( this IApplicationBuilder app, IApiVersionDescriptionProvider provider )
+        {
+            //app.UseMiddleware<SwaggerAuthorizedMiddleware>();
+            app.UseSwagger();
+            app.UseSwaggerUI(
+                options =>
+                {
+                    foreach (var description in provider.ApiVersionDescriptions)
+                    {
+                        options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", description.GroupName.ToUpperInvariant());
+                    }
+                });
+
+            return app;
+        }
+    }
+}
