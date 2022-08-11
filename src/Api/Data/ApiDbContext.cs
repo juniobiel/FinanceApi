ac38048ad8252de99d80f3f2b0638280c8457b8c
Gabriel Junio <gabriel.junio@rocketmail.com>
2022-08-11 16:25:20 -0300

    Updating configs to pgsql

--- src/Api/Data/ApiDbContext.cs
+++ src/Api/Data/ApiDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Api.Data
@@ -6,5 +7,6 @@ namespace Api.Data
     public class ApiDbContext : IdentityDbContext
     {
         public ApiDbContext( DbContextOptions<ApiDbContext> options ) : base(options) { }
+
     }
 }
