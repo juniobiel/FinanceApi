79c21515f485ee1c1693d102d094e5caa64f6e78
Gabriel Junio <gabriel.junio@rocketmail.com>
2022-08-12 01:41:59 -0300

    Initiating AuthController tests implementation

--- src/UnitTests/Auth/AuthControllerTests.cs
+++ src/UnitTests/Auth/AuthControllerTests.cs
@@ -1,4 +1,9 @@
 ﻿using Api.Controllers;
+using Api.Extensions.User.ViewModels;
+using Business.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
 using Moq.AutoMock;
 
 namespace UnitTests.Auth
@@ -6,23 +11,36 @@ namespace UnitTests.Auth
     public class AuthControllerTests
     {
         private readonly AuthController _authService;
-        private readonly AutoMocker mocker;
+        private readonly AutoMocker _mocker;
 
         public AuthControllerTests()
         {
-            mocker = new AutoMocker();
-            _authService = mocker.CreateInstance<AuthController>();
+            _mocker = new AutoMocker();
+            _authService = _mocker.CreateInstance<AuthController>();
         }
 
         [Fact(DisplayName = "Criar conta e retornar sucesso")]
         [Trait("create-account", "sucess with account")]
-        public void ReturnActionResult_StatusCode200_WithCreatedAccount()
+        public async Task ReturnActionResult_StatusCode200_WithCreatedAccount()
         {
             //Arrange
+            var newUser = new RegisterUserViewModel
+            {
+                Email = "teste@teste.com",
+                Password = "teste123456",
+                ConfirmPassword = "teste123456"
+            };
+
+            _mocker.GetMock<UserManager<IdentityUser>>()
+                .Setup(x => x.CreateAsync(It.IsAny<IdentityUser>(), It.IsAny<string>()))
+                .Returns(Task.FromResult(new IdentityResult()));
 
             //Act
+            var result = await _authService.CreateAccount(newUser);
 
             //Assert
+            Assert.IsType<ActionResult<OkResult>>(result);
+
         }
     }
 }
