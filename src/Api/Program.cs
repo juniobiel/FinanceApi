af55d3473dd02b3c8cdc69f613565e704f395029
Gabriel Junio <gabriel.junio@rocketmail.com>
2022-08-16 16:07:35 -0300

    HEROKU atualizando string de conexao

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -14,13 +14,10 @@ namespace Api
 
             var builder = WebApplication.CreateBuilder(args);
 
-            builder.Configuration
-                .SetBasePath(builder.Environment.ContentRootPath)
-                .AddJsonFile("appsettings.json", true, true)
-                .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true)
-                .AddEnvironmentVariables();
-
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+            var IsDevelopment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Development";
+            var connectionString = IsDevelopment ?
+                builder.Configuration.GetConnectionString("DefaultConnection") :
+                GetHerokuConnectionString();
 
             builder.Services.AddApiConfig();
             builder.Services.AddDependenciesInjections();
@@ -42,6 +39,19 @@ namespace Api
             app.UseSwaggerConfig(apiVersionDescriptionProvider);
 
             app.Run();
+        }
+
+        private static string GetHerokuConnectionString()
+        {
+            string connectionUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+            var databaseUri = new Uri(connectionUrl);
+
+            string db = databaseUri.LocalPath.Trim('/');
+            string[] userInfo = databaseUri.UserInfo.Split(':', StringSplitOptions.RemoveEmptyEntries);
+
+            return  $"User ID={userInfo[0]};Password={userInfo[1]};Host={databaseUri.Host};" +
+                    $"Port={databaseUri.Port};Database={db};Pooling=true;" +
+                    $"SSL Mode=Require;Trust Server Certificate=True;";
         }
     }
 
