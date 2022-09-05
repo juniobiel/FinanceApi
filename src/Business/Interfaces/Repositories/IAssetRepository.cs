8bd6907eb0488e5d2f2679f20072993b29072566
Gabriel Junio <gabriel.junio@rocketmail.com>
2022-09-05 19:09:03 -0300

    Regras de negócio para o Repositório

--- src/Business/Interfaces/Repositories/IAssetRepository.cs
+++ src/Business/Interfaces/Repositories/IAssetRepository.cs
@@ -5,5 +5,7 @@ namespace UnitTests.Services
     public interface IAssetRepository
     {
         Task<int> CreateNewAsset( Asset asset );
+        Task<Asset> GetAsset( string ticker );
+        Task<int> UpdateAsset( Asset asset );
     }
 }
---
+++ src/Data/Repositories/AssetRepository.cs
@@ -0,0 +1,23 @@
+﻿using Business.Models;
+using UnitTests.Services;
+
+namespace Data.Repositories
+{
+    public class AssetRepository : IAssetRepository
+    {
+        public Task<int> CreateNewAsset( Asset asset )
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Asset> GetAsset( string ticker )
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> UpdateAsset( Asset asset )
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
