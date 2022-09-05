a4811168c77a81249a9e2c2e736aa1353761b2fd
Gabriel Junio <gabriel.junio@rocketmail.com>
2022-09-05 19:10:06 -0300

    Ajustes para a captura da APIKEY via Injeção de dependencia

--- src/Business/Services/AlphaVantage/AlphaVantageService.cs
+++ src/Business/Services/AlphaVantage/AlphaVantageService.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Services.AlphaVantage.ViewModels;
+using Business.Services.ServiceKey;
 using Flurl.Http;
 using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
@@ -8,18 +9,18 @@ namespace Business.Services.AlphaVantage
     public class AlphaVantageService : IAlphaVantageService
     {
         readonly IConfiguration _configuration;
-        private string ApiKey { get; set; }
+        private IServiceKey _serviceKey;
 
-        public AlphaVantageService( IConfiguration configuration )
+        public AlphaVantageService( IConfiguration configuration, IServiceKey serviceKey )
         {
             _configuration = configuration;
-
-            ApiKey = _configuration["AlphaVantage_ApiKey"];
+            _serviceKey = serviceKey;
+            _serviceKey.AlphaVantageKey = _configuration["AlphaVantage_ApiKey"];
         }
 
         public async Task<AlphaVantageSearchResult> SearchAsset( string ticker )
         {
-            var uri = $"https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords={ticker}.SAO&apikey={ApiKey}";
+            var uri = $"https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords={ticker}.SAO&apikey={_serviceKey.AlphaVantageKey}";
             var result = await uri.GetStringAsync();
             var json = JsonConvert.DeserializeObject<AlphaVantageSearchResult>(result);
 
@@ -28,7 +29,7 @@ namespace Business.Services.AlphaVantage
 
         public async Task<AlphaVantageAssetHistory> GetAssetHistory( string ticker )
         {
-            var uri = $"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={ticker}.SAO&outputsize=full&apikey={ApiKey}";
+            var uri = $"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={ticker}.SAO&outputsize=full&apikey={_serviceKey.AlphaVantageKey}";
             var result = await uri.GetStringAsync();
             var json = JsonConvert.DeserializeObject<AlphaVantageAssetHistory>(result);
 
